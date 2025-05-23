eed83db356cf88c975de9f2da886d0669c3caf50
Joel Bello <Joel8bl@gmail.com>
2025-05-23 15:21:37 +0200

    adding photos

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,48 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all.with_attached_photo
+  end
+
+  def show
+    product
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "Tu producto se ha creado correctamente"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    product.destroy
+    redirect_to products_path, notice: "Tu producto se ha eliminado correctamente"
+  end
+
+  def edit
+    product
+  end
+
+  def update
+    if product.update(product_params)
+      redirect_to products_path, notice: "Tu producto se ha actualizado correctamente"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :price, :description, :photo)
+  end
+
+  def product
+    @product ||= Product.find(params[:id])
+  end
+end

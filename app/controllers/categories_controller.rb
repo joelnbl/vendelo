a1ab888a07b493b5ba17c3418b791c80edf47ccf
Joel Bello <Joel8bl@gmail.com>
2025-05-27 22:36:26 +0200

    Adds categories management feature
    Adds the ability to create, read, update, and delete categories.
    
    This feature enhances the application by allowing users to organize products into different categories.
    It includes the necessary model, controller, views, routes, and tests.

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,44 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+    category
+  end
+
+  def create
+    @category = Category.new(category_params)
+      if @category.save
+        redirect_to categories_url, notice: t(".created")
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
+
+  def update
+      if category.update(category_params)
+        redirect_to categories_url, notice: t(".updated")
+      else
+        render :edit, status: :unprocessable_entity
+      end
+  end
+
+  def destroy
+    category.destroy!
+      redirect_to categories_path, status: :see_other, notice: t(".destroyed")
+  end
+
+  private
+    def category
+      @category = Category.find(params.expect(:id))
+    end
+
+    def category_params
+      params.expect(category: [ :name ])
+    end
+end
